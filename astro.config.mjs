f9368cf3411aa70d03ea002ed64d8ac0c767b1d0
Pierre Lavalley <pierre.lavalley@gmail.com>
2024-04-29 10:46:19 +0200

    add vercel adapter with analytics enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://astro-nutritrack.chrstnl.com/",
@@ -16,5 +18,9 @@ export default defineConfig({
             external: ["svgo"]
         }
     },
-    integrations: [mdx(), sitemap(), tailwind(), prefetch(), react()]
+    integrations: [mdx(), sitemap(), tailwind(), prefetch(), react()],
+    output: "server",
+    adapter: vercel({
+        webAnalytics: {enabled: true}
+    })
 });
